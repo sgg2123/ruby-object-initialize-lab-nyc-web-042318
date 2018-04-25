18b9ae3fe034aa66cee6bd5fc242de0c4789a187
Sydney Garay <sgg2123@columbia.edu>
2018-04-25 21:57:10 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,21 @@
 class Dog
-  def initialize(breed)
+  
+  def initialize(name, breed)
+    @name = name
     if breed == nil
       @breed = "Mutt"
     else
       @breed = breed
     end
   end
+  
+  def name=(name)
+    @name = name
+  end
+ 
+  def name
+    @name
+ 
  
   def breed=(breed)
     @breed = breed
